4af403019c64ad5f1ce22b76cf35898e9be73f54
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-03-14 15:33:44 +0530

    Update pv_extraction_test.groovy

--- pv_extraction_test.groovy
+++ pv_extraction_test.groovy
@@ -1,17 +1,10 @@
-import groovy.json.JsonOutput
-
-def dbhostname
-def destdb
-def dbname(){	
-    if (params.ENVIRONMENT == 'INT'){
-	return db_name = "lpesipo1000int.schneider-electric.com:1521:pesv2int"
-	return credentials_id = "5e3e48a8-651d-48bd-813c-a3735c8af832"  
-    }else if (params.ENVIRONMENT == 'PPR'){
-        db_name = "0lpesipo1000int.schneider-electric.com:1521:pesv2int"
-	credentials_id = "05e3e48a8-651d-48bd-813c-a3735c8af832"
-    }else if (params.ENVIRONMENT == 'PRD'){
-      	db_name = "1lpesipo1000int.schneider-electric.com:1521:pesv2int"
-	credentials_id = "15e3e48a8-651d-48bd-813c-a3735c8af832"
+def dbname(){
+    if (params.ENV_NAME == 'INT'){
+        return [db_name:'lpesipo1000int.schneider-electric.com:1521:pesv2int' , credentials_id:'d0d8e9f8-96c4-42dd-a38b-a549a204dbc9']
+    }else if (params.ENV_NAME == 'PPR'){
+        return [db_name:'lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr' , credentials_id:'3ed29a02-7552-491b-8a4c-d1b1b52d3f71']    
+    }else if (params.ENV_NAME == 'PRD'){
+        return [db_name:'lpesipo1002prd.schneider-electric.com:1521:pesv2prd' , credentials_id:'50319cf9-32dc-49e8-bec3-70010c89b9bb']   
     }
 }
 
@@ -25,14 +18,135 @@ pipeline {
     stages {
         stage("Display the Env variables") {
             steps {
-		    script{
-			    sh '''
-			    	echo $db_name
-				echo $credentials_id
-			    '''
-		    }
-	    }
-	}
-	    
+            script{
+            try {
+                data = dbname()
+                db_name = data.db_name
+                credentials_id = data.credentials_id
+                withCredentials([usernamePassword(credentialsId: credentials_id, passwordVariable: 'CATA_PASS', usernameVariable: 'CATA_USER')])
+                { 
+                  if (ALL_CATALOG == 'true') {
+                      sh '''
+cat <<EOF > se-variants-extractor.yml
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: se-variants-extractor
+  namespace: epam
+spec:
+  ttlSecondsAfterFinished: 20
+  template:
+    metadata:
+      labels:
+        job-name: se-variants-extractor
+    spec:
+      containers:
+      - args:
+        - --spring.data.mongodb.host=se-variants-mongo
+        - --spring.datasource.url=jdbc:oracle:thin:@'''+db_name+'''``
+        - --spring.datasource.username=$CATA_USER
+        - --spring.datasource.password=$CATA_PASS
+        command:
+        - java
+        - -Xms2g
+        - -Xmx6g
+        - -XX:+UseG1GC
+        - -Doracle.jdbc.fanEnabled=false
+        - -jar
+        - /app/product-variants-1.0.0.jar
+        image: registry.us.se.com/epam/product-data-generator
+        imagePullPolicy: Always
+        name: se-variants-extractor
+        resources:
+          requests:
+            memory: "4096Mi"
+            cpu: "1000m"
+          limits:
+            memory: "12288Mi"
+            cpu: "2000m"
+        securityContext:
+          allowPrivilegeEscalation: false
+          capabilities: {}
+          privileged: false
+          readOnlyRootFilesystem: false
+          runAsNonRoot: false
+      imagePullSecrets:
+      - name: se
+      restartPolicy: Never
+EOF
+                      '''.stripIndent()
+                    } else {
+                      sh '''
+cat <<EOF > se-variants-extractor.yml
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: se-variants-extractor
+  namespace: epam
+spec:
+  ttlSecondsAfterFinished: 20
+  template:
+    metadata:
+      labels:
+        job-name: se-variants-extractor
+    spec:
+      containers:
+      - args:
+        - --spring.data.mongodb.host=se-variants-mongo
+        - --spring.datasource.url=jdbc:oracle:thin:@'''+db_name+'''``
+        - --spring.datasource.username=$CATA_USER
+        - --spring.datasource.password=$CATA_PASS
+        - locales=${LOCALES}
+        - sourceIds=${SOURCE_IDS}
+        - type=${TYPE}
+        command:
+        - java
+        - -Xms2g
+        - -Xmx6g
+        - -XX:+UseG1GC
+        - -Doracle.jdbc.fanEnabled=false
+        - -jar
+        - /app/product-variants-1.0.0.jar
+        image: registry.us.se.com/epam/product-data-generator
+        imagePullPolicy: Always
+        name: se-variants-extractor
+        resources:
+          requests:
+            memory: "4096Mi"
+            cpu: "1000m"
+          limits:
+            memory: "12288Mi"
+            cpu: "2000m"
+        securityContext:
+          allowPrivilegeEscalation: false
+          capabilities: {}
+          privileged: false
+          readOnlyRootFilesystem: false
+          runAsNonRoot: false
+      imagePullSecrets:
+      - name: se
+      restartPolicy: Never
+EOF
+                      '''.stripIndent()
+                    }  
+                }
+                } catch (Exception e) {
+                    echo 'Exception occurred: ' + e.toString()
+                    echo 'Envalid DB selected'
+                    currentBuild.result = 'FAILURE'
+                }
+            } 
+            }
+        }
+        stage("File") {
+            when {
+                expression { currentBuild.result != 'FAILURE' }
+            }
+            steps {
+                script{
+                    sh 'cat se-variants-extractor.yml'
+                }
+            }
+        }
     }
 }
