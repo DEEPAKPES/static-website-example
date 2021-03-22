60b0d90968466a676cfbb93b52117ee9cd622045
learn-cloud <51190838+learn-cloud@users.noreply.github.com>
2021-03-22 20:02:01 +0530

    Update step-1.groovy

--- step-1.groovy
+++ step-1.groovy
@@ -1,10 +1,10 @@
 def source_dbname(){
     if (params.SOURCE_ORACLE_ENV == 'INT_ORACLE'){
-        return [db_name:'lpesipo1000int.schneider-electric.com:1521:pesv2int' , credentials_id:'d0d8e9f8-96c4-42dd-a38b-a549a204dbc9']
+        return [db_name:'lpesipo1000int.schneider-electric.com:1521:pesv2int' , credentials_id:'']
     }else if (params.SOURCE_ORACLE_ENV == 'PRE_PROD'){
-        return [db_name:'lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr' , credentials_id:'3ed29a02-7552-491b-8a4c-d1b1b52d3f71']
+        return [db_name:'lpesipo1009ppr.schneider-electric.com:1521:pesv2ppr' , credentials_id:'']
     }else if (params.SOURCE_ORACLE_ENV == 'PROD_STANDBY_DB'){
-        return [db_name:'lpesipo1010prd.schneider-electric.com:1521:pesv2prdstby' , credentials_id:'50319cf9-32dc-49e8-bec3-70010c89b9bb']
+        return [db_name:'lpesipo1010prd.schneider-electric.com:1521:pesv2prdstby' , credentials_id:'']
     }
 }
 
@@ -27,9 +27,7 @@ pipeline {
 
                     data = source_dbname()
                     source_db_name = data.db_name
-                    source_credentials_id = data.credentials_id
                     echo source_db_name
-                    echo source_credentials_id
                 }
             }
         }
@@ -40,10 +38,6 @@ pipeline {
                 data = source_dbname()
                 source_db_name = data.db_name
                 source_credentials_id = data.credentials_id
-
-                dest_db = destination_dbname()
-                destination_db_name = dest_db.db_name
-                dest_credentials_id = dest_db.credentials_id
 
                 withCredentials([usernamePassword(credentialsId: source_credentials_id, passwordVariable: 'CATA_PASS', usernameVariable: 'CATA_USER')])
                 {
